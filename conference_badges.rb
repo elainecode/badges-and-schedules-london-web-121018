f409f026e9396b1192241393b23f6adbb34895c5
I_AM Elaine <elaine.onb@gmail.com>
2018-12-06 18:36:29 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,26 @@
 # Write your code here.
+def batch_badge_creator(arr)
+  arr2 = []
+  arr.each do |name|
+    arr2 << "Hello, my name is #{name}" 
+  end
+  return arr2
+end
+ 
+def assign_rooms(arr)
+ for counter in 1..7 do
+    puts "Hello, #{arr[counter]}! You'll be assigned to room #{counter}!"
+  end
+end 
+
+def printer(arr)
+  assign_rooms(batch_badge_creator(arr))
+  for counter in 1..7 do
+    puts "Hello, #{arr[counter]}! You'll be assigned to room #{counter}!"
+  end
+  arr.each do |name|
+    arr2 << "Hello, my name is #{name}" 
+  end
+
+  
+   
